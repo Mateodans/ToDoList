3918115373b4d84f9c6b730298b959bf2238a346
Mateodans <90775644+Mateodans@users.noreply.github.com>
2022-09-01 23:28:56 -0300

    terminado

--- src/componentes/form.jsx
+++ src/componentes/form.jsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../stylesheets/form.css'
+import { v4 as uuidv4 } from 'uuid'
 
 function TareaForm(props){
+
+    const [input, setInput] = useState('')
+
+    const manejarCambio = e =>{
+        setInput(e.target.value)
+    }
+
+    const manejarEnvio = e =>{
+        e.preventDefault()
+
+        const tareaNueva = {
+            id: uuidv4(),
+            texto: input,
+            completada: false
+        }
+        props.onSubmit(tareaNueva)
+    }
+
     return(
-        <form className='tarea-form'>
+        <form className='tarea-form'
+        onSubmit={manejarEnvio}>
             <input type="text"
                 className='tarea-input'
                 placeholder='Escriba una tarea'
                 name='text'
+                onChange={manejarCambio}
             />
             <button className='tarea-boton'>
                 Agregar Tarea
--- src/componentes/listas.jsx
+++ src/componentes/listas.jsx
@@ -1,13 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TareaForm from './form';
+import Tarea from './tareas';
 import '../stylesheets/lista.css'
 
 function ListaDeTareas() {
+
+    const [tareas, setTareas] = useState([]);
+
+    const agregarTarea = tarea => {
+        if (tarea.texto.trim()) {
+            tarea.texto = tarea.texto.trim()
+
+            const tareasActualizadas = [tarea, ...tareas]
+            setTareas(tareasActualizadas)
+        }
+    }
+
+    const eliminarTarea = id =>{
+        const tareasActualizadas = tareas.filter(tarea => tarea.id !== id)
+        setTareas(tareasActualizadas)
+    }
+
+    const completarTarea = id =>{
+        const tareasActualizadas = tareas.map(tarea => {
+            if (tarea.id === id){
+                tarea.completada = !tarea.completada;
+            }
+            return tarea;
+        })
+        setTareas(tareasActualizadas)
+    }
+
     return (
         <>
-            <TareaForm />
+            <TareaForm onSubmit={agregarTarea} />
             <div className='tareas-listas-contenedor'>
-            lista de tareas
+            {
+                tareas.map((tarea) =>
+                <Tarea
+                    key={tarea.id}
+                    id={tarea.id}
+                    texto={tarea.texto}
+                    completada={tarea.completada}
+                    completarTarea={completarTarea}
+                    eliminarTarea={eliminarTarea}
+
+                    />
+                )
+            }
             </div>
         </>
     )
